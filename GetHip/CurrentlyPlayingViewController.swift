9bf1d93d4c3ec2ce4b752e89b3024a234d398122
Okechi <okechi@Okechis-Mac-mini.local>
2016-02-11 01:52:16 -0500

    Working on party group scene that shows each person in the party

--- GetHip/CurrentlyPlayingViewController.swift
+++ GetHip/CurrentlyPlayingViewController.swift
@@ -7,15 +7,42 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class CurrentlyPlayingViewController: UIViewController {
+    @IBOutlet var songImg: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var artistAndAlbumLabel: UILabel!
+    @IBOutlet var minLabel: UILabel!
+    @IBOutlet var maxLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    
+    //Host buttons
+    @IBOutlet var volCtrl: UISlider!
+    @IBOutlet var ppButton: UIButton!
+    @IBAction func endParty(sender: AnyObject){
+    
+    }
+    
+    //Guest buttons
+    @IBOutlet var favButton: UIButton!
+    @IBAction func favoriteSong(sender: AnyObject){
+    
+    }
 
+
+    
     var party: PartyServiceManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.songImg.image = self.party.currentSong.valueForProperty(MPMediaItemPropertyArtwork).imageWithSize(songImg.frame.size)
+        self.titleLabel.text = (self.party.currentSong.valueForProperty(MPMediaItemPropertyTitle) as? String!)!
+        self.artistAndAlbumLabel.text = (self.party.currentSong.valueForProperty(MPMediaItemPropertyArtist) as? String!)! + " - " + (self.party.currentSong.valueForProperty(MPMediaItemPropertyAlbumTitle) as? String!)!
+        
+
     }
 
     override func didReceiveMemoryWarning() {
